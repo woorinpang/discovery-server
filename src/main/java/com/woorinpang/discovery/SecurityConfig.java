a4d2d514829af50b553d8b24c235ee7eee629c8b
hclee <heechul4296@gmail.com>
2023-02-22 22:36:01 +0900

    project discovery server

---
+++ src/main/java/com/woorinpang/discovery/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.woorinpang.discovery;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity //Spring Security 설정들을 활성화시켜 준다.
+public class SecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/actuator/?*").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and().build();
+    }
+}
