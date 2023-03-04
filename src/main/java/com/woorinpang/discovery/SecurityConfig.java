6121470bb283dace66a2253caea82d561b10093f
hclee <heechul4296@gmail.com>
2023-03-04 22:21:28 +0900

    [Refactor] SecurityConfig 수정 - @Configuration 추가

--- src/main/java/com/woorinpang/discovery/SecurityConfig.java
+++ src/main/java/com/woorinpang/discovery/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.woorinpang.discovery;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity //Spring Security 설정들을 활성화시켜 준다.
 public class SecurityConfig {
 
